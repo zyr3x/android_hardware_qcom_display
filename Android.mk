61d1926962dba27f16444b0a4fbcd646fa5fa9f8
huangqiwu <huangqiwu@shendu.com>
2013-02-18 14:05:02 +0800

    add TARGET_QCOM_DISPLAY_VARIANT

--- Android.mk
+++ Android.mk
@@ -1,9 +1,10 @@
-ifneq ($(filter msm8974 msm8960 msm8660 msm7x27a msm7x27,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8974 msm8960 msm8660 msm7x27a msm7x27 msm7x30,$(TARGET_BOARD_PLATFORM)),)
+ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),)
 display-hals := libgralloc libgenlock libcopybit
 display-hals += libhwcomposer liboverlay libqdutils libexternal libqservice
 ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
 display-hals += liblight
 endif
 include $(call all-named-subdir-makefiles,$(display-hals))
-
+endif
 endif
